221985eb5e99bb9162faffe646dfbdf273853cf6
TIAGO SILVA <frost.tiago@gmail.com>
2021-10-20 10:26:07 -0300

    Adicionar arquivos de projeto.

---
+++ AppXamarinFlixNoturno/AppXamarinFlixNoturno/Filmes/CapitanMarvel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXamarinFlixNoturno.Filmes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CapitanMarvel : ContentPage
+    {
+        public CapitanMarvel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppXamarinFlixNoturno/AppXamarinFlixNoturno/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppXamarinFlixNoturno
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Categorias.Aventura());
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
